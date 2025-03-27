ed9244dce983efcb64ed8f33a7cb67d327d94229
Muhammad Fouly <muhammadfouly@gmail.com>
2025-03-27 17:51:03 -0400

    Novice complete

--- src/main/java/edu/unc/comp301/Base.java
+++ src/main/java/edu/unc/comp301/Base.java
@@ -1,18 +1,43 @@
 package edu.unc.comp301;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Base implements IBase{
+    private int supplyCount;
+    private Lock lock;
+    public Base(){
+        supplyCount = 0;
+        lock = new ReentrantLock();
+    }
     @Override
-    public void addSupplies(int amount) {
+    public synchronized void addSupplies(int amount) {
+        if (amount <= 0){
+            throw new IllegalArgumentException();
+        }
+        supplyCount += amount;
+        System.out.println("added " + amount + "supplies. Total: " + supplyCount);
 
     }
 
     @Override
-    public int getSupplyCount() {
-        return 0;
+    public synchronized int getSupplyCount() {
+        return supplyCount;
     }
 
     @Override
     public void useTool(String task) {
+        lock.lock();
+        try{
+            System.out.println("Survivor is using the tool");
+            System.out.println(task);
+            Thread.sleep(1000);
+            System.out.println("The tool is no longer in use");
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            lock.unlock();
+        }
 
     }
 }
--- src/main/java/edu/unc/comp301/Survivor.java
+++ src/main/java/edu/unc/comp301/Survivor.java
@@ -1,13 +1,50 @@
 package edu.unc.comp301;
 
 public class Survivor implements ISurvivor{
+    private Base base;
+    private volatile boolean stopFlag;
+    public Survivor(Base base){
+        this.base = base;
+        this.stopFlag = false;
+    }
     @Override
     public void stop() {
-
+        this.stopFlag = true;
     }
-
     @Override
     public void run() {
-
+        this.stopFlag = false;
+        while(!stopFlag){
+            try{
+                this.performAction();
+            }catch (InterruptedException e) {
+                this.stop();
+            }
+        }
     }
+    protected void performAction() throws InterruptedException {
+        int random = (int)(Math.random() * 3) + 1;
+        switch (random){
+            case 1 -> this.fortify();
+            case 2 -> this.scavenge();
+            case 3 -> this.rest();
+        }
+    }
+    protected void fortify() throws InterruptedException {
+        base.useTool("fortification");
+        System.out.println("Fortifying the base");
+        Thread.sleep(1000);
+    }
+    protected void scavenge() throws InterruptedException {
+        System.out.println("Survivor is scavenging");
+        int randomSleep = (int)(Math.random() * 4) + 1;
+        Thread.sleep(1000 * randomSleep);
+        int randomSupplies = (int)(Math.random() * 10) + 1;
+        base.addSupplies(randomSupplies);
+    }
+    protected void rest() throws InterruptedException {
+        System.out.println("Survivor is taking a sleep");
+        Thread.sleep(2000);
+    }
+
 }
