4b5cd52badf1fc5a09613211334db3b277906704
Muhammad Fouly <muhammadfouly@gmail.com>
2025-03-29 03:09:47 -0400

    Jedi complete

--- src/main/java/edu/unc/comp301/Base.java
+++ src/main/java/edu/unc/comp301/Base.java
@@ -3,55 +3,57 @@ package edu.unc.comp301;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
-public class Base implements IBase{
-    private int supplyCount;
-    private Lock lock;
-    private volatile boolean isUnderAttack;
-    public Base(){
-        supplyCount = 0;
-        lock = new ReentrantLock();
-    }
-    @Override
-    public synchronized void addSupplies(int amount) {
-        if (amount <= 0){
-            throw new IllegalArgumentException();
-        }
-        supplyCount += amount;
-        System.out.println(Thread.currentThread().getName() + " added " + amount + "supplies. Total: " + supplyCount);
+public class Base implements IBase {
+  private int supplyCount;
+  private Lock lock;
+  private volatile boolean isUnderAttack;
 
-    }
+  public Base() {
+    supplyCount = 0;
+    lock = new ReentrantLock();
+  }
 
-    @Override
-    public synchronized int getSupplyCount() {
-        return supplyCount;
+  @Override
+  public synchronized void addSupplies(int amount) {
+    if (amount <= 0) {
+      throw new IllegalArgumentException();
     }
+    supplyCount += amount;
+    System.out.println(
+        Thread.currentThread().getName() + " added " + amount + "supplies. Total: " + supplyCount);
+  }
 
-    @Override
-    public void useTool(String task) {
-        lock.lock();
-        try{
-            System.out.println(Thread.currentThread().getName() + " is using the tool");
-            System.out.println(task);
-            Thread.sleep(1000);
-            System.out.println("The tool is no longer in use");
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        } finally {
-            lock.unlock();
-        }
+  @Override
+  public synchronized int getSupplyCount() {
+    return supplyCount;
+  }
 
+  @Override
+  public void useTool(String task) {
+    lock.lock();
+    try {
+      System.out.println(Thread.currentThread().getName() + " is using the tool");
+      System.out.println(task);
+      Thread.sleep(1000);
+      System.out.println("The tool is no longer in use");
+    } catch (InterruptedException e) {
+      throw new RuntimeException(e);
+    } finally {
+      lock.unlock();
     }
-    public synchronized void startAttack(){
-        this.isUnderAttack = true;
-        System.out.println("Zombies are approaching");
+  }
 
-    }
-    public synchronized void endAttack(){
-        this.isUnderAttack = false;
-        System.out.println("The survivors have successfully repelled the zombies.");
+  public synchronized void startAttack() {
+    this.isUnderAttack = true;
+    System.out.println("Zombies are approaching");
+  }
 
-    }
-    public synchronized boolean isUnderAttack(){
-        return this.isUnderAttack;
-    }
+  public synchronized void endAttack() {
+    this.isUnderAttack = false;
+    System.out.println("The survivors have successfully repelled the zombies.");
+  }
+
+  public synchronized boolean isUnderAttack() {
+    return this.isUnderAttack;
+  }
 }
--- src/main/java/edu/unc/comp301/RandomZombieAttacks.java
+++ src/main/java/edu/unc/comp301/RandomZombieAttacks.java
@@ -1,17 +1,17 @@
 package edu.unc.comp301;
 
-public class RandomZombieAttacks implements IDayStrategy{
-    @Override
-    public void execute(Base base, int durationMilliseconds) throws InterruptedException {
-        int unit = durationMilliseconds / 5;
-        Thread.sleep(unit);
-        base.startAttack();
-        Thread.sleep(unit);
-        base.endAttack();
-        Thread.sleep(unit);
-        base.startAttack();
-        Thread.sleep(unit);
-        base.endAttack();
-        Thread.sleep(unit);
-    }
+public class RandomZombieAttacks implements IDayStrategy {
+  @Override
+  public void execute(Base base, int durationMilliseconds) throws InterruptedException {
+    int unit = durationMilliseconds / 5;
+    Thread.sleep(unit);
+    base.startAttack();
+    Thread.sleep(unit);
+    base.endAttack();
+    Thread.sleep(unit);
+    base.startAttack();
+    Thread.sleep(unit);
+    base.endAttack();
+    Thread.sleep(unit);
+  }
 }
